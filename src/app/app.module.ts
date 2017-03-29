8d62391d1ad21f3e3fdbefb75981910a7934e270
Per Karlsson <per@pkarlsson.com>
2017-03-29 14:39:58 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDeg1p2lTESeyZHqtOanEjfNklKxCGtVOQ",
+    authDomain: "angular4-f74ac.firebaseapp.com",
+    databaseURL: "https://angular4-f74ac.firebaseio.com",
+    storageBucket: "angular4-f74ac.appspot.com",
+    messagingSenderId: "1042628695410"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
